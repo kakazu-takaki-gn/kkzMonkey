f34381e14fd5aafff0038de120b514efab1af370
嘉数 貴明 <kakazu_takaki_gn@cyberagent.co.jp>
2018-07-05 14:32:46 +0900

    テスト通るところまで

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -26,13 +26,13 @@ func TestNextToken(t *testing.T) {
 	l := New(input)
 
 	for i, tt := range tests {
-		tok := l.NextToken
+		tok := l.NextToken()
 		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected= % q, got = %q", i, tt.expectedType, tok().Type)
+			t.Fatalf("tests[%d] - tokentype wrong. expected= % q, got = %q", i, tt.expectedType, tok.Type)
 		}
 
 		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected = %q, got = %q", i, tt.expectedLiteral, tok().Literal)
+			t.Fatalf("tests[%d] - literal wrong. expected = %q, got = %q", i, tt.expectedLiteral, tok.Literal)
 		}
 	}
 
